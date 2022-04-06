840f45d013649ae9c85bbb76eefebc8573daf981
alopine <loariel98@gmail.com>
2022-04-05 18:02:43 -0700

    Add JavaScript logic to change cell color on mouseover

--- etch.js
+++ etch.js
@@ -1,7 +1,19 @@
+// Select container
 const container = document.querySelector("#container");
 
+// Add grid cells to container
 for (i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.setAttribute("class", "cell");
     container.appendChild(div);
 }
+
+// Select cells
+const cells = document.querySelectorAll(".cell");
+
+// Logic for changing cell colors on hover
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "black";
+    });
+});
